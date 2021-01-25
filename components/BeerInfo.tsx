dc014827d819b4ca7707952c5c1c8385686e2928
codyarose <crose992@gmail.com>
2021-01-25 00:12:56 -0600

    feat: :sparkles: add BeerInfo component, refactor BeerList to use it

---
+++ components/BeerInfo.tsx
@@ -0,0 +1,177 @@
+import React, { FC } from "react"
+import Link from "next/link"
+import styled from "@emotion/styled"
+import { css } from "@emotion/react"
+import { Beer } from "../mocks/handlers"
+import {
+	useCartDispatch,
+	useCartState,
+	CartActionType,
+} from "../context/CartContext"
+import { buttonReset } from "../styles/utils"
+
+interface Props {
+	beer: Beer
+	layout?: "card" | "full"
+}
+
+const BeerInfo: FC<Props> = ({ beer, layout = "full" }) => {
+	const dispatch = useCartDispatch()
+	const { cart } = useCartState()
+	const handleAddToCart = (id: number) => {
+		dispatch({ type: CartActionType.addItem, payload: { id, qty: 1 } })
+	}
+	const handleDecrement = (id: number) => {
+		dispatch({ type: CartActionType.decrement, payload: { id } })
+	}
+
+	const getCartItem = (id: number) => {
+		return cart.find((item) => item.id === id)
+	}
+
+	const { id, image, name, style, abv, price, description } = beer
+
+	const cartItem = getCartItem(id)
+	const isInCart = Boolean(cartItem)
+
+	return (
+		<Item layout={layout} key={id}>
+			<ItemImage src={image} alt={`${name} label`} />
+			<ItemBody layout={layout}>
+				<ItemTitle>
+					<Link href={`/${id}`}>
+						<a>{name}</a>
+					</Link>
+				</ItemTitle>
+				<ItemDetails layout={layout}>
+					<small>Style: {style}</small>
+					<small>ABV: {abv}%</small>
+				</ItemDetails>
+				{description ? <p>{description}</p> : null}
+				<ItemPrice>${price}</ItemPrice>
+				<CartButtons>
+					{isInCart && (
+						<CartDecrement onClick={() => handleDecrement(id)}>
+							-
+						</CartDecrement>
+					)}
+					<CartAdd
+						isInCart={isInCart}
+						onClick={() => handleAddToCart(id)}
+					>
+						{isInCart ? `${cartItem.qty} added` : "add to cart"}
+					</CartAdd>
+				</CartButtons>
+			</ItemBody>
+		</Item>
+	)
+}
+const Item = styled.div<Partial<Props>>`
+	${({ layout }) =>
+		layout === "card"
+			? `
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	padding: 1.5rem 1rem;
+	`
+			: null}
+	${({ layout }) =>
+		layout === "full"
+			? `
+		display: grid;
+		grid-template-columns: 100px auto;
+		gap: 1rem;
+		padding: 2rem;
+		`
+			: null}
+	border: 1px solid lightgray;
+	border-radius: 3px;
+`
+
+const ItemImage = styled.img`
+	width: 100px;
+	height: 100px;
+	object-fit: cover;
+	object-position: center;
+`
+
+const ItemBody = styled.div<Partial<Props>>`
+	width: 100%;
+	display: grid;
+	grid-template-rows: repeat(4, auto);
+	gap: 1rem;
+	margin: 1rem 0 0;
+	${({ layout }) =>
+		layout === "full"
+			? `
+		align-items: flex-start;
+	`
+			: null}
+`
+
+const ItemTitle = styled.h2`
+	font-size: 1.25rem;
+	margin-bottom: 0;
+`
+
+const ItemDetails = styled.div<Partial<Props>>`
+	display: grid;
+	gap: 1rem;
+	white-space: nowrap;
+	grid-template-columns: ${({ layout }) =>
+		layout === "card"
+			? `
+		repeat(2, 1fr)
+	`
+			: `repeat(2, min-content)`};
+`
+
+const ItemPrice = styled.span`
+	font-size: 1.5rem;
+`
+
+const CartButtons = styled.div`
+	display: grid;
+	grid-template-columns: min-content 1fr;
+	width: 100%;
+	max-width: 300px;
+`
+
+export const buttonStyles = css`
+	${buttonReset}
+	background-color: var(--button-bg);
+	&:hover {
+		background-color: var(--button-bg-hover);
+	}
+	&:focus {
+		background-color: var(--button-bg-hover);
+	}
+`
+
+const CartAdd = styled.button<{ isInCart: boolean }>`
+	${buttonStyles}
+	position: relative;
+	grid-column: 2 / 3;
+	${({ isInCart }) =>
+		isInCart
+			? `
+	padding-left: 0;
+	padding-right: 2rem;
+	`
+			: null}
+	&:before {
+		${({ isInCart }) => (isInCart ? "content: '+'" : "content: ''")};
+		position: absolute;
+		top: 50%;
+		right: 1rem;
+		transform: translateY(-50%);
+	}
+`
+const CartDecrement = styled.button`
+	${buttonStyles}
+`
+
+export default BeerInfo
--- components/BeerList.tsx
+++ components/BeerList.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from "react"
 import { gql, useQuery } from "@apollo/client"
-import Link from "next/link"
 import styled from "@emotion/styled"
-import {
-	CartActionType,
-	useCartDispatch,
-	useCartState,
-} from "../context/CartContext"
 import { Beer } from "../mocks/handlers"
+import BeerInfo from "./BeerInfo"
 
 interface Props {
 	nameFilter: string
@@ -27,8 +22,6 @@ export const GET_BEERS = gql`
 		}
 	}
 `
-const imageFallback =
-	"https://untappd.akamaized.net/site/assets/images/temp/badge-beer-default.png"
 
 const BeerList: FC<Props> = ({ nameFilter, styleFilter }) => {
 	const { loading, error, data } = useQuery<{ beers: Beer[]; count: number }>(
@@ -38,15 +31,6 @@ const BeerList: FC<Props> = ({ nameFilter, styleFilter }) => {
 		}
 	)
 	const { count, beers } = data ?? {}
-	const dispatch = useCartDispatch()
-	const { cart } = useCartState()
-	const handleAddToCart = (id: number) => {
-		dispatch({ type: CartActionType.addItem, payload: { id } })
-	}
-
-	const isInCart = (id: number): boolean => {
-		return cart.find((item) => item.id === id)
-	}
 
 	return (
 		<div>
@@ -56,41 +40,11 @@ const BeerList: FC<Props> = ({ nameFilter, styleFilter }) => {
 				<>
 					<span>{count} results</span>
 					<List>
-						{beers.map((beer: Beer) => {
-							const { id, name, image, style, price, abv } = beer
-							const displayedImage = image ? image : imageFallback
-							return (
-								<Item key={id}>
-									<ItemImage
-										src={displayedImage}
-										alt={`${name} label`}
-									/>
-									<ItemBody>
-										<ItemTitle>
-											<Link
-												href={`/${id}`}
-												// as={`/${toKebabCase(name)}`}
-											>
-												<a>{name}</a>
-											</Link>
-										</ItemTitle>
-										<ItemDetails>
-											<small>Style: {style}</small>
-											<small>ABV: {abv}</small>
-										</ItemDetails>
-										<ItemPrice>${price}</ItemPrice>
-										<button
-											onClick={() => handleAddToCart(id)}
-											disabled={isInCart(id)}
-										>
-											{isInCart(id)
-												? "added to cart"
-												: "add to cart"}
-										</button>
-									</ItemBody>
-								</Item>
-							)
-						})}
+						{beers.map((beer: Beer) => (
+							<li key={beer.id}>
+								<BeerInfo beer={beer} layout='card' />
+							</li>
+						))}
 					</List>
 				</>
 			)}
@@ -106,45 +60,6 @@ const List = styled.ul`
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
 	gap: 2rem;
-`
-
-const Item = styled.li`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding: 1.5rem 1rem;
-	border: 1px solid lightgray;
-	border-radius: 3px;
-`
-
-const ItemImage = styled.img`
-	width: 100px;
-	height: 100px;
-	object-fit: cover;
-	object-position: center;
-`
-
-const ItemBody = styled.div`
-	display: flex;
-	flex-direction: column;
-	text-align: center;
-	margin: 1rem 0 0;
-`
-
-const ItemTitle = styled.h2`
-	font-size: 1.25rem;
-	margin-bottom: 0.5rem;
-`
-
-const ItemDetails = styled.div`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	gap: 0.5rem;
-	margin-bottom: 0.5rem;
-`
-
-const ItemPrice = styled.span`
-	font-size: 1.5rem;
 `
 
 export default BeerList
